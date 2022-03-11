f6f203ada575f57bb1335f3af62def9d71736a4a
IEatSushi <2klaus.4peter@gmail.com>
2022-03-11 16:09:15 +0100

    distribute images to workers

--- template_matching_mpi.cpp
+++ template_matching_mpi.cpp
@@ -5,7 +5,6 @@
 #include "stb_image.h"
 #include "stb_image_write.h"
 
-
 float calc_pixels_mean_value(unsigned char** img, int w, int h, int x = 0, int y = 0) {
 	int pixel_sum = 0;
 	for (int dy = 0; dy < h; ++dy) {
@@ -44,19 +43,15 @@ int calc_pixels_abs_a_minus_b_sum(unsigned char **img_a, unsigned char **patch_b
 			pixel_sum += abs(img_a[img_x + x][img_y + y] - patch_b[x][y]);
 		}
 	}
-	if (img_x < 5 && img_y < 5) {
-		printf("%i\n", pixel_sum);
-	}
 	return pixel_sum;
 }
 
-void match_patch(unsigned char** img, int img_w, unsigned char** patch, int patch_w, int patch_h, int start_y, int end_y, int part, int cor[3]) {
-
+void match_patch(unsigned char** img, int img_w, int img_h, unsigned char** patch, int patch_w, int patch_h, int cor[3]) {
 	int max_correlation = 9999999;
 	int correlation_x = -1;
 	int correlation_y = -1;
 
-	for (int y = start_y; y <= end_y - patch_h; ++y) {
+	for (int y = 0; y <= img_h - patch_h; ++y) {
 		for (int x = 0; x <= img_w - patch_w; ++x) {
 			int correlation = calc_pixels_abs_a_minus_b_sum(img, patch, patch_w, patch_h, x, y);
 
@@ -70,7 +65,6 @@ void match_patch(unsigned char** img, int img_w, unsigned char** patch, int patc
 	cor[0] = max_correlation;
 	cor[1] = correlation_x;
 	cor[2] = correlation_y;
-	printf("maximum of patch %i: %i, %i\n", part, correlation_x, correlation_y);
 }
 
 // https://stackoverflow.com/questions/61410931/write-a-c-program-to-convert-1d-array-to-2d-array-using-pointers Besucht: 03.03.2022
@@ -85,8 +79,8 @@ void array_to_matrix(unsigned char** matrix, const unsigned char* arr, int cols,
 
 unsigned char** alloc_mat(int cols, int rows) {
 	unsigned char** A1, * A2;
-	A1 = (unsigned char**)calloc(cols, sizeof(unsigned char*));     // pointer on columns
-	A2 = (unsigned char*)calloc(rows * cols, sizeof(unsigned char));    // all matrix elements
+	A1 = (unsigned char**) calloc(cols, sizeof(unsigned char*));     // pointer on columns
+	A2 = (unsigned char*) calloc(rows * cols, sizeof(unsigned char));    // all matrix elements
 	for (int x = 0; x < cols; ++x) {
 		A1[x] = A2 + x * rows;
 	}
@@ -97,102 +91,102 @@ int main(int argc, char** argv) {
 	char *const img_path = argv[1];
 	char *const patch_path = argv[2];
 
-	int processID, commSize;
+	int processID, comm_size;
 	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &commSize);
+	MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &processID);
 	MPI_Status status;
 
 	int offsetTag = 1;
 
-	int img_w = 0;
-	int img_h = 0;
-	int img_c = 0; // number of image channels
+	int img_w;
+	int img_h;
+	int img_c; // number of image channels
 	int desired_c = 1;
 
 	unsigned char* img = NULL;
-	//load RGB image as 1 channel grayscale image (1x unsigned 8 bit per pixel)
-	img = stbi_load("../search_area_small.jpg", &img_w, &img_h, &img_c, desired_c);
+
+	int patch_w;
+	int patch_h;
+	int patch_c;
+	unsigned char* patch = NULL;
+
+	unsigned char** img2d;
+	unsigned char** patch2d;
+
+	int offset;
+
 	if (processID == 0) {
+		//load RGB image as 1 channel grayscale image (1x unsigned 8 bit per pixel)
+		img = stbi_load(img_path, &img_w, &img_h, &img_c, desired_c);
 		printf("\nLoaded image: %s\n", (img != NULL ? "true" : "false"));
 		printf("\twidth: %d\n", img_w);
 		printf("\theight: %d\n", img_h);
-	}
-
-	int patch_w = 0;
-	int patch_h = 0;
-	int patch_c = 0;
-	unsigned char* patch = NULL;
-	patch = stbi_load("../nemo_template.png", &patch_w, &patch_h, &patch_c, desired_c);
-	if (processID == 0) {
+		patch = stbi_load(patch_path, &patch_w, &patch_h, &patch_c, desired_c);
 		printf("\nLoaded patch: %s\n", (patch != NULL ? "true" : "false"));
 		printf("\twidth: %d\n", patch_w);
 		printf("\theight: %d\n", patch_h);
-	}
 
-	unsigned char** img2d = alloc_mat(img_w, img_h);
-	unsigned char** patch2d = alloc_mat(patch_w, patch_h);
+		img2d = alloc_mat(img_w, img_h);
+		patch2d = alloc_mat(patch_w, patch_h);
+		array_to_matrix(img2d, img, img_w, img_h);
+		array_to_matrix(patch2d, patch, patch_w, patch_h);
 
-	array_to_matrix(img2d, img, img_w, img_h);
-	array_to_matrix(patch2d, patch, patch_w, patch_h);
-
-	int offset_y;
-	int worker_count;
-	int start_y;
-	int end_y;
-
-	int w_max_correlation;
-	int w_correlation_x;
-	int w_correlation_y;
-
-	if (processID == 0) {
 		double start, end;
 		start = MPI_Wtime();
 
-		offset_y = img_h / commSize;
-		start_y = 0;
-		worker_count = commSize - 1;
-		end_y = img_h - offset_y * worker_count;
+		int worker_count = comm_size - 1;
+		int wholePart = (img_w - patch_w) / worker_count;
+		int remainder = (img_w - patch_w) % worker_count;
+		offset = 0;
 
-		for (int workerID = 1; workerID < commSize; ++workerID) {
-			start_y += offset_y;
-			end_y += offset_y;
-			MPI_Send(&start_y, 1, MPI_INT, workerID, offsetTag, MPI_COMM_WORLD);
-			MPI_Send(&end_y, 1, MPI_INT, workerID, offsetTag, MPI_COMM_WORLD);
+		for (int workerID = 1; workerID < comm_size; ++workerID) {
+			int segment_width = workerID <= remainder ? wholePart + 1 : wholePart;
+			int col_count = segment_width + (patch_w - 1);
+
+			MPI_Send(&patch_w, 1, MPI_INT, workerID, offsetTag, MPI_COMM_WORLD);
+			MPI_Send(&patch_h, 1, MPI_INT, workerID, offsetTag, MPI_COMM_WORLD);
+			MPI_Send(&patch2d[0][0], patch_w * patch_h, MPI_UNSIGNED_CHAR, workerID, offsetTag, MPI_COMM_WORLD);
+
+			MPI_Send(&col_count, 1, MPI_INT, workerID, offsetTag, MPI_COMM_WORLD);
+			MPI_Send(&img_h, 1, MPI_INT, workerID, offsetTag, MPI_COMM_WORLD);
+			MPI_Send(&offset, 1, MPI_INT, workerID, offsetTag, MPI_COMM_WORLD);
+			MPI_Send(&img2d[offset][0], col_count * img_h, MPI_UNSIGNED_CHAR, workerID, offsetTag, MPI_COMM_WORLD);
+			offset += segment_width;
 		}
+		int max_correlation[3] = {999999, -1, -1};
 
-		int max_cor[3];
-		match_patch(img2d, img_w, patch2d, patch_w, patch_h, 0, img_h - offset_y * worker_count, 0, max_cor);
-		int max_correlation = max_cor[0];
-		int max_correlation_x = max_cor[1];
-		int max_correlation_y = max_cor[2];
+		for (int workerID = 1; workerID < comm_size; ++workerID) {
+			int worker_correlation[3];
+			MPI_Recv(&worker_correlation[0], 3, MPI_INT, workerID, offsetTag, MPI_COMM_WORLD, &status);
 
-		for (int workerID = 1; workerID < commSize; ++workerID) {
-			MPI_Recv(&w_max_correlation, 1, MPI_INT, workerID, offsetTag, MPI_COMM_WORLD, &status);
-			MPI_Recv(&w_correlation_x, 1, MPI_INT, workerID, offsetTag, MPI_COMM_WORLD, &status);
-			MPI_Recv(&w_correlation_y, 1, MPI_INT, workerID, offsetTag, MPI_COMM_WORLD, &status);
-			if (w_max_correlation < max_correlation) {
-				max_correlation = w_max_correlation;
-				max_correlation_x = w_correlation_x;
-				max_correlation_y = w_correlation_y;
+			if (worker_correlation[0] < max_correlation[0]) {
+				max_correlation[0] = worker_correlation[0];
+				max_correlation[1] = worker_correlation[1];
+				max_correlation[2] = worker_correlation[2];
 			}
 		}
 		end = MPI_Wtime();
+		printf("Found nemo at x: %i y: %i\n", max_correlation[1], max_correlation[2]);
 		printf("Task took %fs to complete.\n", end - start);
-		printf("Found nemo at x: %i y: %i\n", max_correlation_x, max_correlation_y);
 	}
 	if (processID != 0) {
-		MPI_Recv(&start_y, 1, MPI_INT, 0, offsetTag, MPI_COMM_WORLD, &status);
-		MPI_Recv(&end_y, 1, MPI_INT, 0, offsetTag, MPI_COMM_WORLD, &status);
+		MPI_Recv(&patch_w, 1, MPI_INT, 0, offsetTag, MPI_COMM_WORLD, &status);
+		MPI_Recv(&patch_h, 1, MPI_INT, 0, offsetTag, MPI_COMM_WORLD, &status);
+		patch2d = alloc_mat(patch_w, patch_h);
+		MPI_Recv(&patch2d[0][0], patch_w * patch_h, MPI_UNSIGNED_CHAR, 0, offsetTag, MPI_COMM_WORLD, &status);
 
-		int cor_w[3];
-		match_patch(img2d, img_w, patch2d, patch_w, patch_h, start_y, end_y, processID, cor_w);
-		w_max_correlation = cor_w[0];
-		w_correlation_x = cor_w[1];
-		w_correlation_y = cor_w[2];
-		MPI_Send(&w_max_correlation, 1, MPI_INT, 0, offsetTag, MPI_COMM_WORLD);
-		MPI_Send(&w_correlation_x, 1, MPI_INT, 0, offsetTag, MPI_COMM_WORLD);
-		MPI_Send(&w_correlation_y, 1, MPI_INT, 0, offsetTag, MPI_COMM_WORLD);
+		MPI_Recv(&img_w, 1, MPI_INT, 0, offsetTag, MPI_COMM_WORLD, &status);
+		MPI_Recv(&img_h, 1, MPI_INT, 0, offsetTag, MPI_COMM_WORLD, &status);
+		MPI_Recv(&offset, 1, MPI_INT, 0, offsetTag, MPI_COMM_WORLD, &status);
+		img2d = alloc_mat(img_w, img_h);
+		MPI_Recv(&img2d[0][0], img_w * img_h, MPI_UNSIGNED_CHAR, 0, offsetTag, MPI_COMM_WORLD, &status);
+
+		int worker_correlation[3];
+		match_patch(img2d, img_w, img_h, patch2d, patch_w, patch_h, worker_correlation);
+		worker_correlation[1] += offset;
+
+		MPI_Send(&worker_correlation[0], 3, MPI_INT, 0, offsetTag, MPI_COMM_WORLD);
 	}
 	MPI_Finalize();
 }
--- template_matching_mpi_old.cpp
+++ template_matching_mpi_old.cpp
@@ -5,18 +5,39 @@
 #include "stb_image.h"
 #include "stb_image_write.h"
 
-void crop(unsigned char** matrix, int x, int y, int w, int h, const char* fileName) {
-	unsigned char* patch = (unsigned char*)calloc(w * h, sizeof(unsigned char));
-	int k = 0;
+
+float calc_pixels_mean_value(unsigned char** img, int w, int h, int x = 0, int y = 0) {
+	int pixel_sum = 0;
 	for (int dy = 0; dy < h; ++dy) {
 		for (int dx = 0; dx < w; ++dx) {
-			patch[k++] = matrix[x + dx][y + dy];
+			pixel_sum += img[x + dx][y + dy];
 		}
 	}
-	stbi_write_jpg(fileName, w, h, 1, patch, 100);
+	return 1.0f * pixel_sum / (w * h);
 }
 
-int calc_pixels_abs_a_minus_b_sum(unsigned char** img_a, unsigned char** patch_b, int w, int h, int img_x, int img_y) {
+int calc_pixels_a_times_b_sum(unsigned char** img_a, unsigned char** patch_b, int w, int h, int img_x, int img_y) {
+	int pixel_sum = 0;
+	for (int dy = 0; dy < h; ++dy) {
+		for (int dx = 0; dx < w; ++dx) {
+			pixel_sum += img_a[img_x + dx][img_y + dy] * patch_b[dx][dy];
+		}
+	}
+	return pixel_sum;
+}
+
+int calc_pixels_squared_sum(unsigned char** img, int w, int h, int x = 0, int y = 0) {
+	int pixels_squared_sum = 0;
+	for (int dy = 0; dy < h; ++dy) {
+		for (int dx = 0; dx < w; ++dx) {
+			int pixel = img[x + dx][y + dy];
+			pixels_squared_sum += pixel * pixel;
+		}
+	}
+	return pixels_squared_sum;
+}
+
+int calc_pixels_abs_a_minus_b_sum(unsigned char **img_a, unsigned char **patch_b, int w, int h, int img_x, int img_y) {
 	int pixel_sum = 0;
 	for (int y = 0; y < h; ++y) {
 		for (int x = 0; x < w; ++x) {
@@ -26,7 +47,7 @@ int calc_pixels_abs_a_minus_b_sum(unsigned char** img_a, unsigned char** patch_b
 	return pixel_sum;
 }
 
-int* match_patch(unsigned char** img, int img_w, int img_h, unsigned char** patch, int patch_w, int patch_h, int start_y, int end_y, int part) {
+void match_patch(unsigned char** img, int img_w, unsigned char** patch, int patch_w, int patch_h, int start_y, int end_y, int part, int cor[3]) {
 
 	int max_correlation = 9999999;
 	int correlation_x = -1;
@@ -43,12 +64,10 @@ int* match_patch(unsigned char** img, int img_w, int img_h, unsigned char** patc
 			}
 		}
 	}
-	int cor[3];
 	cor[0] = max_correlation;
 	cor[1] = correlation_x;
 	cor[2] = correlation_y;
 	printf("maximum of patch %i: %i, %i\n", part, correlation_x, correlation_y);
-	return(cor);
 }
 
 // https://stackoverflow.com/questions/61410931/write-a-c-program-to-convert-1d-array-to-2d-array-using-pointers Besucht: 03.03.2022
@@ -72,6 +91,9 @@ unsigned char** alloc_mat(int cols, int rows) {
 }
 
 int main(int argc, char** argv) {
+	char *const img_path = argv[1];
+	char *const patch_path = argv[2];
+
 	int processID, commSize;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &commSize);
@@ -85,6 +107,7 @@ int main(int argc, char** argv) {
 	int img_c = 0; // number of image channels
 	int desired_c = 1;
 
+	
 	unsigned char* img = NULL;
 	//load RGB image as 1 channel grayscale image (1x unsigned 8 bit per pixel)
 	img = stbi_load("../search_area_small.jpg", &img_w, &img_h, &img_c, desired_c);
@@ -111,7 +134,6 @@ int main(int argc, char** argv) {
 	array_to_matrix(img2d, img, img_w, img_h);
 	array_to_matrix(patch2d, patch, patch_w, patch_h);
 
-	int offset_x;
 	int offset_y;
 	int worker_count;
 	int start_y;
@@ -125,7 +147,6 @@ int main(int argc, char** argv) {
 		double start, end;
 		start = MPI_Wtime();
 
-		offset_x = 0;
 		offset_y = img_h / commSize;
 		start_y = 0;
 		worker_count = commSize - 1;
@@ -138,8 +159,8 @@ int main(int argc, char** argv) {
 			MPI_Send(&end_y, 1, MPI_INT, workerID, offsetTag, MPI_COMM_WORLD);
 		}
 
-		int* max_cor = match_patch(img2d, img_w, img_h, patch2d, patch_w, patch_h, 0, img_h - offset_y * worker_count, 0);
-
+		int max_cor[3];
+		match_patch(img2d, img_w, patch2d, patch_w, patch_h, 0, img_h - offset_y * worker_count, 0, max_cor);
 		int max_correlation = max_cor[0];
 		int max_correlation_x = max_cor[1];
 		int max_correlation_y = max_cor[2];
@@ -156,13 +177,14 @@ int main(int argc, char** argv) {
 		}
 		end = MPI_Wtime();
 		printf("Task took %fs to complete.\n", end - start);
-		printf("overall maximum: %i, %i\n", max_correlation_x, max_correlation_y);
-		crop(img2d, max_correlation_x, max_correlation_y, patch_w, patch_h, "nemo_found.jpg");
+		printf("Found nemo at x: %i y: %i\n", max_correlation_x, max_correlation_y);
 	}
 	if (processID != 0) {
 		MPI_Recv(&start_y, 1, MPI_INT, 0, offsetTag, MPI_COMM_WORLD, &status);
 		MPI_Recv(&end_y, 1, MPI_INT, 0, offsetTag, MPI_COMM_WORLD, &status);
-		int* cor_w = match_patch(img2d, img_w, img_h, patch2d, patch_w, patch_h, start_y, end_y, processID);
+
+		int cor_w[3];
+		match_patch(img2d, img_w, patch2d, patch_w, patch_h, start_y, end_y, processID, cor_w);
 		w_max_correlation = cor_w[0];
 		w_correlation_x = cor_w[1];
 		w_correlation_y = cor_w[2];
